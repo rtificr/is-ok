265e2aa04db8e74a625d492c180fb781442cd940
Jay Churchill <102625530+rtificr@users.noreply.github.com>
2024-10-11 23:51:32 -0400

    Is everything ok?
    Is everything ok?

---
+++ main.py
@@ -0,0 +1,7 @@
+import time
+
+print("Is everything ok?")
+while True:
+  if (1+1) != 2:
+    print("God help you")
+  time.sleep(1) #reduces your carbon emissions, leading to a better future for humanity
